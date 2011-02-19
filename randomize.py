7108a6eb6a9de1f3df0d80f479d6cdf82109b197
Caner Candan <caner@candan.fr>
2011-02-20 00:08:43 +0100

    added additionnal options

--- randomize.py
+++ randomize.py
@@ -34,20 +34,22 @@ import common
 
 logger = logging.getLogger("Randomizer")
 
-logger.info("TEST")
+parser = optparse.OptionParser()
+parser.add_option('-p', '--plot', action='store_true', default=False, help='plot data')
+parser.add_option('-r', '--nruns', type='int', default=1, help='give here a number of runs')
+parser.add_option('-b', '--nblocks', type='int', default=128, help='give here a number of blocks to use')
+parser.add_option('-t', '--nthreads', type='int', default=64, help='give here a number of threads to use')
+options = common.parser(parser)
 
-options = common.parser()
+if options.plot:
+    import pylab
 
 def justtest():
-    blocks = 64
-    block_size = 128
-    nbr_values = blocks * block_size
+    nbr_values = options.nblocks * options.nthreads
 
-    logger.info("Using nbr_values == %d" % nbr_values)
-
-    n_iter = 100000
-
-    logger.info("Calculating %d iterations" % n_iter)
+    logger.info("Parameters:")
+    logger.info("- nbr_values = %d" % nbr_values)
+    logger.info("- nruns = %d" % options.nruns)
 
     start = drv.Event()
     end = drv.Event()
@@ -58,23 +60,37 @@ def justtest():
     for loop all in C call from Python
     """
     kernel = ElementwiseKernel(
-        "float *data, int n_iter",
-        #"for(int n = 0; n < n_iter; n++) { data[i] = sin(data[i]);}",
-        #"for(int n = 0; n < n_iter; n++) { data[i] = i;}",
-        "data[i] = erfc(data[i]);",
+        "float* data, int nruns",
+        """
+        for (int r = 0; r < nruns; ++r)
+        {
+            data[i] = erfc(data[i]);
+        }
+        """,
         "gpuerfc")
+
+    mtkernel = ElementwiseKernel(
+        "float* data, int nPerRng",
+        """
+        int iState, iState1, iStateM, iOut;
+        unsigned int mti, mti1, mtiM, x;
+        unsigned int mt[MT_NN], matrix_a, mask_b, mask_c;
+        """,
+        "mtrand")
 
     data = gpuarray.to_gpu( numpy.ones(nbr_values).astype(numpy.float32) )
 
     start.record()
-    kernel( data, numpy.int(n_iter) )
+    kernel( data, options.nruns )
     end.record()
 
     end.synchronize()
     secs = start.time_till(end)*1e-3
 
+    hostdata = data.get()
+
     logger.info("Elementwise time and first three results:")
-    logger.info("%fs, %s" % (secs, str(data.get()[:3])))
+    logger.info("%fs, %s, %d" % (secs, str(hostdata[:3]), len(hostdata)))
 
 def main():
     justtest()
