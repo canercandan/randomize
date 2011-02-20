2a8440571c7195255547b279c9a68f96ecb45d58
Caner Candan <caner@candan.fr>
2011-02-20 16:32:31 +0100

    + added a new test file using randomize library classes

---
+++ test/t-sampler.cu
@@ -0,0 +1,55 @@
+// -*- mode: c++; c-indent-level: 4; c++-member-init-indent: 8; comment-column: 35; -*-
+
+/* This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ * Caner Candan <caner@candan.fr>, http://caner.candan.fr
+ */
+
+#include <randomize/randomize>
+
+using namespace core_library;
+using namespace randomize;
+
+typedef float Atom;
+
+int main(void)
+{
+    NormalMono< Atom > distribution( 10, 10 );
+    //Uniform< Atom > distribution( 10, 10 );
+
+    // MTNormalMono< Atom > rng;
+    // //MTUniform< Atom > rng;
+    // //XorshiftNormalMono< Atom > rng;
+    // //XorshiftUniform< Atom > rng;
+
+    // SamplerNormalMono< Atom > sampler( rng );
+    // //SamplerUniform< Atom > sampler( rng );
+
+    // // Only for normal distribution samples:
+    // ErfInv< Atom > inverse;
+    // //BoxMuller< Atom > inverse;
+    // //Marsaglia< Atom > inverse;
+
+    // Vector< Atom > sample( 100 );
+    // //Matrix< Atom > sample( 100, 100 );
+    // //Data< Atom > sample;
+
+    // sampler( distribution, sample );
+    // inverse( sample );
+
+    // logger << sample << std::endl;
+
+    return 0;
+}
